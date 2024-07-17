430c605f27793344a81f9ae6976e8b3ee7a1bf5d
Kaushik035 <kaushikmandal629@gmail.com>
2024-07-17 13:32:04 +0530

    upto lec 5 done

---
+++ L4/main.py
@@ -0,0 +1,3 @@
+print('hello world',55)
+print(5)
+print(6565*2)
---
+++ L5/main.py
@@ -0,0 +1,10 @@
+print("Kaushik is going to be one of the \
+futuristic coder in the entire world ")
+
+print("""uheaufsuf9ubgsdufsugbfdugsufu suahfugsaugfuogaufgoua suashfuohasoufous aiufasf ushfiusa fhsaugfugasufiusagfugsu  afu uhafuh auhfouaa fouhsoufgusahofugowaffougaouf ouf saoufh oushafougsou fs fuhaf a""")
+
+
+
+print("jbfbajbf ajbhjhf a f ahfu f \"hjalfjs\" njbjbbkjsbg")
+
+
