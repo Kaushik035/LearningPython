06d71073786439938971724459786fcd12aec5cf
Kaushik035 <kaushikmandal629@gmail.com>
2024-07-25 00:12:27 +0530

    account exercise completed of oops p6 in 1st part

--- p6_accountExer.py
+++ p6_accountExer.py
@@ -5,4 +5,18 @@ class Account:
 
     def debit(self,amount):
         self.balance = self.balance - amount
-        return self.balance
+        print(f"Rupees {amount} is debited from the account",'\n',f"Available balence is rupees {self.balance}")
+    
+    def credit(self,amount):
+        self.balance = self.balance + amount
+        print(f"Rupees {amount} is credited to the account",'\n',f"Available balence is rupees {self.balance}")
+
+    def get_balance(self):
+        print(f"available balance is {self.balance}")
+
+acc = Account(10000,422)
+acc.get_balance()
+acc.debit(5000)
+acc.credit(2000)
+
+acc.get_balance()
